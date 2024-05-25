3db75b360025654c663f395a33ad7289d6a1771e
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-26 05:07:26 +0800

    Exo 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+""" In this module, we typically manage Squares """
+
 
 class square():
-    
+    """ Here the class for Squares """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Square initialissation procedure """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Here for sure we manage the square perimeter """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Getting the str representation of a square """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
